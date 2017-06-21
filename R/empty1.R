e9e961a54dc7ae222e6a573a314de4cc20003d24
Steve Lane <lane.s@unimelb.edu.au>
2017-06-21 15:24:15 +1000

    Add test R scripts

---
+++ R/empty1.R
@@ -0,0 +1,5 @@
+## A script that does nothing but attaches packages and calculates 2+2
+library(ggplot2)
+library(dplyr)
+require(tidyr)
+2+2
---
+++ R/empty2.R
@@ -0,0 +1,4 @@
+## A script that does nothing but attaches packages and calculates 3+4
+library(ggplot2)
+require(lme4)
+3+4
