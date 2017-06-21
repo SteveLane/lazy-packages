0e7f2b990d297e56bbe9a65293498948a2f10153
Steve Lane <lane.s@unimelb.edu.au>
2017-06-21 15:24:26 +1000

    Missing opening brace.

--- scripts/ipak.R
+++ scripts/ipak.R
@@ -7,14 +7,14 @@ args <- commandArgs(trailingOnly = TRUE)
 ## Date: Wednesday, 21 June 2017
 ## Synopsis: Script to test for installed packages, and if not installed,
 ## install them.
-## Time-stamp: <2017-06-21 14:46:27 (slane)>
+## Time-stamp: <2017-06-21 15:23:56 (slane)>
 ################################################################################
 ################################################################################
 if(!(length(args) %in% 1:2)){
     stop("A single argument is required to be passed to ipak.R: insts.\ninsts is the file location of a newline separated list of required packages.\nYou can also pass in repos, which is the repository you want to install from.\nExample: Rscript ipak.R insts=../installs.txt repos='https://cran.csiro.au/'",
          call. = FALSE)
 } else {
-    if(length(args) == 1)
+    if(length(args) == 1){
         repos <- "https://cran.csiro.au/"
     }
     hasOpt <- grepl("=", args)
