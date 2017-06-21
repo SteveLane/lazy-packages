5dd2321ff6c18febde96740b23a18d6a9e713821
Steve Lane <lane.s@unimelb.edu.au>
2017-06-21 14:46:32 +1000

    Allow setting of repository.
    Disallow github installs via this script.

--- scripts/ipak.R
+++ scripts/ipak.R
@@ -7,13 +7,16 @@ args <- commandArgs(trailingOnly = TRUE)
 ## Date: Wednesday, 21 June 2017
 ## Synopsis: Script to test for installed packages, and if not installed,
 ## install them.
-## Time-stamp: <2017-06-21 12:14:21 (slane)>
+## Time-stamp: <2017-06-21 14:46:27 (slane)>
 ################################################################################
 ################################################################################
-if(!(length(args) == 1)){
-    stop("A single argument must be passed to ipak.R: insts.\ninsts is the location of a newline separated list of required packages.\nExample: Rscript ipak.R insts=../installs.txt",
+if(!(length(args) %in% 1:2)){
+    stop("A single argument is required to be passed to ipak.R: insts.\ninsts is the file location of a newline separated list of required packages.\nYou can also pass in repos, which is the repository you want to install from.\nExample: Rscript ipak.R insts=../installs.txt repos='https://cran.csiro.au/'",
          call. = FALSE)
 } else {
+    if(length(args) == 1)
+        repos <- "https://cran.csiro.au/"
+    }
     hasOpt <- grepl("=", args)
     argLocal <- strsplit(args[hasOpt], "=")
     for(i in seq_along(argLocal)){
@@ -28,15 +31,7 @@ if(!(length(args) == 1)){
     }
 }
 pkg <- scan(insts, "character")
-## Check for github packages (throw away github username)
-chk.git <- gsub(".*/", "", pkg)    
-new.pkg <- pkg[!(chk.git %in% installed.packages()[, "Package"])]
-if(!(length(new.pkg) == 0)){
-    git.ind <- grep("/", new.pkg)
-    if(length(git.ind) == 0){
-        install.packages(new.pkg, dependencies = TRUE,
-                         repos = "https://cran.csiro.au/")
-    } else {
-        devtools::install_github(new.pkg[git.ind])
-    }
+new.pkgs <- pkg[!(pkg %in% installed.packages()[, "Package"])]
+if(!(length(new.pkgs) == 0)){
+    install.packages(new.pkgs, dependencies = TRUE, repos = repos)
 }
